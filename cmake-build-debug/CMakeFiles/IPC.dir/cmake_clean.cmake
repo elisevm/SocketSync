a6fb5412dbacbedc13edfa8cade5db0f222b7bf6
tessatiu <gadjiew.rasim@gmail.com>
2023-10-01 09:41:42 +0300

    sync

---
+++ cmake-build-debug/CMakeFiles/IPC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPC.dir/main.cpp.o"
+  "IPC"
+  "IPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
